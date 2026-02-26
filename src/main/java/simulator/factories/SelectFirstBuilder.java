38bfff0e0a91990edb3ba239f94b92550529947e
jaime <jaimeescribano544@gmail.com>
2026-02-26 10:47:12 +0100

    Subiendo archivos de factories actualizados

--- src/main/java/simulator/factories/SelectFirstBuilder.java
+++ src/main/java/simulator/factories/SelectFirstBuilder.java
@@ -1,5 +1,18 @@
 package simulator.factories;
 
-public class SelectFirstBuilder {
+import org.json.JSONObject;
+
+import simulator.model.SelectFirst;
+import simulator.model.SelectionStrategy;
+
+public class SelectFirstBuilder extends Builder<SelectionStrategy>{
+    public SelectFirstBuilder(){
+        super("first", "Select First Strategy");
+    }
+
+    protected SelectionStrategy createInstance(JSONObject data) {
+        return new SelectFirst();
+    }
+    
     
 }
--- src/main/java/simulator/factories/WolfBuilder.java
+++ src/main/java/simulator/factories/WolfBuilder.java
@@ -1,5 +1,43 @@
 package simulator.factories;
 
-public class WolfBuilder {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import simulator.misc.Utils;
+import simulator.misc.Vector2D;
+import simulator.model.Animal;
+import simulator.model.SelectFirst;
+import simulator.model.SelectionStrategy;
+import simulator.model.Wolf;
+
+public class WolfBuilder extends Builder<Animal>{
+    private Factory<SelectionStrategy> strategyFactory;
+
+    public WolfBuilder(Factory<SelectionStrategy> strategyFactory){
+        super("wolf", "Wolf Builder");
+        this.strategyFactory = strategyFactory;
+    }
+
+    protected Animal createInstance(JSONObject data){
+        SelectionStrategy mateStrategy = new SelectFirst();
+        SelectionStrategy huntStrategy = new SelectFirst();
+
+        if(data.has("mate_strategy")) mateStrategy = strategyFactory.createInstance(data.getJSONObject("mate_strategy"));
+        if(data.has("hunt_strategy")) huntStrategy = strategyFactory.createInstance(data.getJSONObject("hunt_strategy"));
+
+        Vector2D pos = null;
+        if(data.has("pos")){
+            JSONObject p = data.getJSONObject("pos");
+            JSONArray x_range = p.getJSONArray("x_range");
+            JSONArray y_range = p.getJSONArray("y_range");
+
+            double x = x_range.getDouble(0) + Utils.RAND.nextDouble() * (x_range.getDouble(1) - x_range.getDouble(0));
+            double y = y_range.getDouble(0) + Utils.RAND.nextDouble() * (y_range.getDouble(1) - y_range.getDouble(0));
+            pos = new Vector2D(x,y);
+        }
+
+        return new Wolf(mateStrategy, huntStrategy, pos);
+    }
+
     
 }
